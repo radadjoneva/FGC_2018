c80f7e166ae9e6f0d11d9c354350aec9deb232e9
radadjoneva <radadjoneva@gmail.com>
2018-07-08 14:21:28 +0300

    finally moving with 45 degreed wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
@@ -1,17 +1,82 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-
-
-
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.ThreadPool;
 
 @TeleOp(name="My First Op Mode 2", group="Practice-Bot")
+
 public class FirstOpMode extends LinearOpMode {
 
-    public void runOpMode() throws InterruptedException {
-        while(true){
+    private DcMotor leftFrontMotor;
+    private DcMotor rightFrontMotor;
+    private DcMotor leftBackMotor;
+    private DcMotor rightBackMotor;
+    private ElapsedTime period = new ElapsedTime();
 
+    private void waitForTick(long periodMs) throws java.lang.InterruptedException {
+        long remaining = periodMs - (long)period.milliseconds();
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            Thread.sleep(remaining);
+        }
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+    public void runOpMode() throws InterruptedException {
+        //variables
+
+        //map motors
+        leftFrontMotor = hardwareMap.dcMotor.get("frontLeft");
+        rightFrontMotor = hardwareMap.dcMotor.get("frontRight");
+        leftBackMotor = hardwareMap.dcMotor.get("backLeft");
+        rightBackMotor = hardwareMap.dcMotor.get("backRight");
+
+        //initialize classes
+        Move_robot move = new Move_robot("move", leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor);
+
+        //reverse motors
+        rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //set all motors to zero
+        leftFrontMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        leftBackMotor.setPower(0);
+        rightBackMotor.setPower(0);
+
+        //Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        try {
+// run until the end of the match (driver presses STOP)
+            //start classes
+            move.start();
+
+            while(opModeIsActive()) {
+                //set joystick buttons
+                move.setXY(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+                move.rotateRobot(gamepad1.left_bumper, gamepad1.right_bumper);
+
+                waitForTick(40);
+            }
+        } catch (InterruptedException exc) {
+
+            return;
+
+        } finally {
+            //exterminate classes
+            move.exterminate();
+
+            //set all motors to zero power
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            leftBackMotor.setPower(0);
+            rightBackMotor.setPower(0);
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move_robot.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ThreadPool;
+
+public class Move_robot extends Thread {
+    private Thread t;
+    private String threadName;
+    private DcMotor leftFrontMotor;
+    private DcMotor rightFrontMotor;
+    private DcMotor leftBackMotor;
+    private DcMotor rightBackMotor;
+    private double X;
+    private double Y;
+    private double powerLF;
+    private double powerLB;
+    private boolean leftBumper;
+    private boolean rightBumper;
+    private boolean exterminate;
+
+    //constructor
+    Move_robot (String name, DcMotor motorLF, DcMotor motorRF, DcMotor motorLB, DcMotor motorRB){
+        threadName = name;
+        leftFrontMotor = motorLF;
+        rightFrontMotor = motorRF;
+        leftBackMotor = motorLB;
+        rightBackMotor = motorRB;
+    }
+
+    public void start(){
+        if (t == null){
+            t = new Thread(this, threadName);
+            exterminate = false;
+            t.start();
+        }
+    }
+
+    public void exterminate(){
+        exterminate = true;
+    }
+
+    public void setXY(double x, double y){
+        X = x;
+        Y = y;
+    }
+
+    public void rotateRobot(boolean LBumper, boolean RBumper){
+        leftBumper = LBumper;
+        rightBumper = RBumper;
+        if (leftBumper) {
+            leftFrontMotor.setPower(1);
+            rightFrontMotor.setPower(-1);
+            leftBackMotor.setPower(1);
+            rightBackMotor.setPower(-1);
+        }
+        if (rightBumper) {
+            leftFrontMotor.setPower(-1);
+            rightFrontMotor.setPower(1);
+            leftBackMotor.setPower(-1);
+            rightBackMotor.setPower(1);
+        }
+        else{
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            leftBackMotor.setPower(0);
+            rightBackMotor.setPower(0);
+        }
+    }
+
+    public void run(){
+        try {
+            while(!exterminate) {
+                powerLF = (X + Y);
+                powerLB = (Y - X);
+                leftFrontMotor.setPower(powerLF);
+                rightFrontMotor.setPower(powerLB);
+                leftBackMotor.setPower(powerLF);
+                rightBackMotor.setPower(powerLB);
+            }
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            leftBackMotor.setPower(0);
+            rightBackMotor.setPower(0);
+        } catch (Exception e){
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            leftBackMotor.setPower(0);
+            rightBackMotor.setPower(0);
+        }
+    }
+}
